1037a31baa621258f0e7e50c279d8d406b912f0a
steve <steve@DESKTOP-E197DDI.localdomain>
2020-12-04 11:00:53 +0000

    day4tidy

--- 04/part1.pl
+++ 04/part1.pl
@@ -1,15 +1,12 @@
 #!/usr/bin/perl
 while(<>) {
-    chomp;
     $buf .= " $_";
     if(!/\S/ || eof){ 
         $c=0;
         for $tag (qw/ecl pid eyr hcl byr iyr hgt/) {
              $c+= ($buf =~ m/$tag:/);
         }
-        if($c==7) {
-            $x++
-        }
+        $x++ if $c==7;
         $buf='';
     }
 }
