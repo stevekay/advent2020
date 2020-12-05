a144b6e594f92c0a2dfaa95c04271797a5cc26a3
steve kay <steve@>
2020-12-05 11:43:53 +0000

    day5perl, golfed

--- 05/both.pl
+++ 05/both.pl
@@ -1,32 +1,18 @@
 #!/usr/bin/perl
 while(<>) {
- $hi=127;
- $row=0;
- foreach $c (split(//,substr($_,0,7))) {
-  if($c eq 'F') {
-   $hi=($hi-1-$row)/2+$row;
-   next;
-  }
-  $row=($hi+1-$row)/2+$row;
+ $r=$c=0;
+ $s=127;
+ $d=7;
+ foreach(split(//,substr($_,0,10))){
+  $s=($s-1-$r)/2+$r if /F/;
+  $r=($s+1-$r)/2+$r if /B/;
+  $d=($d-1-$c)/2+$c if /L/;
+  $c=($d+1-$c)/2+$c if /R/;
  }
-
- $hi=7;
- $col=0;
- foreach $c (split(//,substr($_,7,3))) {
-  if($c eq 'L') {
-   $hi=($hi-1-$col)/2+$col;
-   next;
-  }
-  $col=($hi+1-$col)/2+$col;
- }
-
- $id=$row*8+$col;
- if($id > $max) { $max=$id }
- $a[$id]='x';
+ $_=$r*8+$c;
+ if($_>$max){$max=$_}
+ $a[$_]='x'
 }
-print "highest=$max\n";
-for $b ( 1 .. ($#a)-1 ) {
- if($a[$b] ne 'x' && $a[$b-1] eq 'x' && $a[$b+1] eq 'x') {
-  print "your seat=$b\n" 
- }
+for$b(1..$#a-1){
+ print"$max $b"if($a[$b] ne 'x'&&$a[$b-1]&&$a[$b+1])
 }
