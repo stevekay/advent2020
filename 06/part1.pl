c4d7083e378cf4814b213ee45e02f9d3c7d7f276
steve kay <steve@>
2020-12-06 10:28:54 +0000

    day6pl1 tidyup

--- 06/part1.pl
+++ 06/part1.pl
@@ -1,15 +1,13 @@
 #!/usr/bin/perl
 while(<>) {
  chomp;
-
- $buf .= " $_";
- if(!/\S/ || eof){
-  undef %h;
-  foreach $c (split(//,$buf)) {
-   if($c =~ m/^[a-z]/) { $h{$c}='x' }
-  }
-  $t+=scalar keys %h;
-  $buf='';
+ $b .= " $_";
+ next unless !/\S/ || eof;
+ undef %h;
+ foreach(split(//,$b)) {
+  $h{$_} = 'x' if /\w/
  }
+ $t+=scalar keys %h;
+ $b=''
 }
-die $t;
+die $t
