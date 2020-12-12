310251dd235910eb2c45c2f5881a1f300f595b2f
steve kay <steve@>
2020-12-12 14:10:20 +0000

    tidyup

--- 12/part1.pl
+++ 12/part1.pl
@@ -1,19 +1,18 @@
 #!/usr/bin/perl
 $facing ='E';
 while(<>) {
- if(/^(.)(\d+)/) {
-  $dir = $1 eq 'F' ? $facing : $1;
-  if($dir eq 'N') { $y += $2 }
-  if($dir eq 'S') { $y -= $2 }
-  if($dir eq 'E') { $x += $2 }
-  if($dir eq 'W') { $x -= $2 }
-  if($dir eq 'L' || $dir eq 'R') {
-   $b = index('NESW',$facing);
-   if($dir eq 'L') { $b -= $2 / 90 }
-   if($dir eq 'R') { $b += $2 / 90 }
-   if($b < 0) { $b = 4+$b }
-   $facing = substr('NESW',$b % 4,1);
-  }
+ next unless /^(.)(\d+)/;
+ $dir = $1 eq 'F' ? $facing : $1;
+ if($dir eq 'N') { $y += $2 }
+ if($dir eq 'S') { $y -= $2 }
+ if($dir eq 'E') { $x += $2 }
+ if($dir eq 'W') { $x -= $2 }
+ if($dir eq 'L' || $dir eq 'R') {
+  $b = index('NESW',$facing);
+  if($dir eq 'L') { $b -= $2 / 90 }
+  if($dir eq 'R') { $b += $2 / 90 }
+  if($b < 0) { $b = 4 + $b }
+  $facing = substr('NESW',$b % 4,1);
  }
 }
 die abs($x)+abs($y)
