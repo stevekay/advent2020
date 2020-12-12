e3c7f588262dd3963d19e0969006437cce6ceda7
steve kay <steve@>
2020-12-12 14:03:05 +0000

    day12pl1 tidyup2

--- 12/part1.pl
+++ 12/part1.pl
@@ -1,29 +1,19 @@
 #!/usr/bin/perl
 $facing ='E';
 while(<>) {
- if(/^([NSEWF])(\d+)/) {
-  $dir = $1;
-  $dist = $2;
-
-  if($dir eq 'F') { $dir = $facing }
-
-  if($dir eq 'N') { $y += $dist }
-  if($dir eq 'S') { $y -= $dist }
-  if($dir eq 'E') { $x += $dist }
-  if($dir eq 'W') { $x -= $dist }
- } 
-
- if(/^([LR])(\d+)/) {
-  $turn = $1;
-  $b = index('NESW',$facing);
-
-  if($turn eq 'L') { $b -= $2 / 90 }
-  if($turn eq 'R') { $b += $2 / 90 }
-  if($b<0) { $b = 4+$b }
-
-  $b = $b % 4;
-  $facing = substr('NESW',$b,1);
+ if(/^(.)(\d+)/) {
+  $dir = $1 eq 'F' ? $facing : $1;
+  if($dir eq 'N') { $y += $2 }
+  if($dir eq 'S') { $y -= $2 }
+  if($dir eq 'E') { $x += $2 }
+  if($dir eq 'W') { $x -= $2 }
+  if($dir eq 'L' || $dir eq 'R') {
+   $b = index('NESW',$facing);
+   if($dir eq 'L') { $b -= $2 / 90 }
+   if($dir eq 'R') { $b += $2 / 90 }
+   if($b < 0) { $b = 4+$b }
+   $facing = substr('NESW',$b % 4,1);
+  }
  }
 }
-
 die abs($x)+abs($y)
